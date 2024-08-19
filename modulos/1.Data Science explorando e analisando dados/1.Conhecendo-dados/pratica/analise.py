2cdde71cc2b8718145a17ca7b3afc82549a950d9
robertdouglasaimon <robertdouglasaimon@gmail.com>
2024-08-19 12:13:06 -0300

    UPDATE

--- modulos/1.Data Science explorando e analisando dados/1.Conhecendo-dados/pratica/analise.py
+++ modulos/1.Data Science explorando e analisando dados/1.Conhecendo-dados/pratica/analise.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Carregar o dataset online
 notas = pd.read_csv('https://raw.githubusercontent.com/alura-cursos/data-science-analise-exploratoria/main/Aula_0/ml-latest-small/ratings.csv')
@@ -26,5 +28,25 @@ notas['nota'].value_counts()
 notas['nota'].mean()
 
 
-print(notas['nota'].mean())  # Nunca apagar esse print, ele quem renderiza os comandos dentro dos parenteses dele.
+
+# notas['nota'].plot(kind='hist')
+# plt.show()  # <-- Isso vai exibir o gráfico do comando "notas['nota'].plot(kind='hist')". Mas cheque se a biblioteca foi importada lá no topo corretamente, caso contrário não funcionará. 
+# Exemplo de como deve estar corretamente importado: import matplotlib.pyplot as plt
+
+
+notas['nota'].median() #Tira a média (O meio EXATO do valor da coluna selecionada para analisar) exata do valor da tabela. 
+
+#Usando template string no Python para chamar a Média(.mean()) e a Médiana (.median()) da coluna "nota":
+mediana = notas ['nota'].median()
+media = notas ['nota'].mean()
+print(f'A mediana é {mediana} e a média é {media}')
+
+notas['nota'].describe() # Pede para o Python descreve a coluna selecionada 'nota' no minimos detalhes.
+print(notas['nota'].describe())
+
+
+# Biblioteca e graficos que mostram visualmente como está distribuido esse valor. Como ver a mediana visualmente e não só em números. Usaremos a biblioteca seaborn atráves do "import seaborn as sns" usando a sigla "sns" para ativa-lá. Lembrando que é preciso instalar ela no terminal através do "pip install seaborn" e importar ela lá no topo com o "import seaborn as sns" para ai sim usar os comandos abaixo para gerar o gráfico.
+# sns.boxplot(notas['nota'])
+# plt.show()
+
 
